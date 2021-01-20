2258b3a7a8a5f578f336115317be65f30a2dd4c0
Andres Davila <andres.davila-g@mail.escuelaing.edu.co>
2021-01-20 09:51:42 -0500

    primera parte completada

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -11,8 +11,12 @@ package edu.eci.arsw.threads;
 public class CountThreadsMain {
     
     public static void main(String a[]){
-        Thread t= new Thread (new CountThread(5,10));
-        t.start();
+        Thread t= new Thread (new CountThread(0,99));
+        Thread t1= new Thread (new CountThread(99,199));
+        Thread t2= new Thread (new CountThread(200,299));
+        t.run();
+        t1.run();
+        t2.run();
     }
     
 }
